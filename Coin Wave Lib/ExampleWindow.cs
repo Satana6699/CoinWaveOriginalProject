3a45edcb1535e34baee42301aa4d29aaeca04b52
Satana <135997444+Satana6699@users.noreply.github.com>
2024-03-20 22:17:13 +0300

    создание новых шейдерных программ и шейдеров. Небольшая структуризация классов.

--- Coin Wave Lib/ExampleWindow.cs
+++ Coin Wave Lib/ExampleWindow.cs
@@ -2,20 +2,34 @@
 using OpenTK;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
-using OpenTK.Graphics.OpenGL;
+using OpenTK.Graphics.OpenGL4;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 using static Coin_Wave_Lib.Player;
+using System;
+using System.Collections.Generic;
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System.IO;
 
 namespace Coin_Wave_Lib
 {
     public class ExampleWindow : GameWindow
     {
+        float[] vertices =
+        {
+            //Position    Texture coordinates
+             0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
+             0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+            -0.5f,  0.5f, 0.0f, 0.0f, 1.0f  // top left
+        };
         KeyboardState lastKeyboardState, currentKeyboardState;
         private float frameTime = 0.0f;
         private int fps = 0;
         Player player;
-
         uint[] player_indexes = new uint[] {
                 0, 1, 2,
                 0, 2, 3,
@@ -107,6 +121,25 @@ namespace Coin_Wave_Lib
             player = new(player_position, 12, velocity, velocity);
             //CreateVAO(map, map_indexes);
             CreateVAO(map, map_indexes);
+
+            string path = @"Assets\data\trava.jpg";
+            //Load the image
+            Image<Rgba32> image = SixLabors.ImageSharp.Image.Load<Rgba32>(path);
+
+            //ImageSharp loads from the top-left pixel, whereas OpenGL loads from the bottom-left, causing the texture to be flipped vertically.
+            //This will correct that, making the texture display properly.
+            image.Mutate(x => x.Flip(FlipMode.Vertical));
+
+            //Use the CopyPixelDataTo function from ImageSharp to copy all of the bytes from the image into an array that we can give to OpenGL.
+            var pixels = new byte[4 * image.Width * image.Height];
+            image.CopyPixelDataTo(pixels);
+            GL.TexImage2D(TextureTarget.Texture2D, 0,
+                PixelInternalFormat.Rgba, image.Width,
+                image.Height, 0, PixelFormat.Rgba,
+                PixelType.UnsignedByte, pixels);
+            // генерация мап карт
+            //GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+
         }
 
         protected override void OnResize(ResizeEventArgs e)
---
+++ Coin Wave Lib/TextureProgram.cs
@@ -0,0 +1,30 @@
+﻿using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+using SixLabors.ImageSharp.Processing;
+
+namespace Coin_Wave_Lib
+{
+    public class TextureProgram
+    {
+        public byte[] GetPixels(string path)
+        {
+            //Load the image
+            Image<Rgba32> image = Image.Load<Rgba32>(path);
+
+            //ImageSharp loads from the top-left pixel, whereas OpenGL loads from the bottom-left, causing the texture to be flipped vertically.
+            //This will correct that, making the texture display properly.
+            image.Mutate(x => x.Flip(FlipMode.Vertical));
+
+            //Use the CopyPixelDataTo function from ImageSharp to copy all of the bytes from the image into an array that we can give to OpenGL.
+            var pixels = new byte[4 * image.Width * image.Height];
+            image.CopyPixelDataTo(pixels);
+            return pixels;
+        }
+    }
+}
